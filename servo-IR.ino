800ee2403f160b7ab5eb206c730226cf03eb6731
Alfred <alfredgithinji87@gmail.com>
2022-07-12 12:09:43 +0300

    First trial Obstacle avoidance

--- servo-IR.ino
+++ servo-IR.ino
@@ -19,10 +19,26 @@ Servo IR_servo;
 #define Button_P 0xFFC23D
 #define Moving_Forward 0xFF02FD
 #define Moving_Backward 0xFF22DD
+
 #define Power 0xFFA25D
 #define Mute 0xFFE21D
-#define Button_Dec 0xFF22DD
-#define Button_Dec 0xFF22DD
+#define Mode 0xFFB04F
+#define Music 0xFF906F
+#define Loop 0xFF6897
+
+#define Button_One 0xFF42BD
+#define Button_Two 0xFF4AB5 
+#define Button_Three 0xFF52AD
+#define Button_Four 0xFF10EF
+#define Button_Five 0xFF38C7
+#define Button_Six 0xFF5AA5
+#define Button_Seven 0xFF30CF
+#define Button_Eight 0xFF18E7
+#define Button_Nine 0xFF7A85
+#define Button_Zero 0xFF9867
+
+
+
 
 //IR receiver & the servo position
 //=================================
@@ -30,8 +46,8 @@ int init_pos = 42;//This is the initial servo position.
 int init2_pos = 90;//THis is the initial position for the ultrasonic sensor.
 int servo_pos = 3;//This is the pin to which the servo is attached to 
 int servo2_pos = 9;//This is the pin attached to the ultrasonic servo
-int c_speed = 100;//THis is for the speed increment and decrement.
-int b_speed = 100;//This is the constant speed for reversing.
+int c_speed = 120;//THis is for the speed increment and decrement.
+int b_speed = 120;//This is the constant speed for reversing.
 
 //IR receiver variables
 //======================
@@ -40,6 +56,7 @@ uint32_t Previous;
 
 //DC motor variables
 //===================
+bool direction_tracker = true;
 int enA = 5;
 int in1 = 2;
 int in2 = 4;
@@ -122,14 +139,27 @@ int speedIncrement (int new_speed){
 
   void  inc_speed(){
     c_speed = speedIncrement(c_speed);
+    if(direction_tracker==true){
+    Serial.print("Forward Direction at speed ;  ");
     Serial.println(c_speed);
     movingForward(c_speed);
-    };
+    }else if (direction_tracker==false){
+    Serial.print("Backward Direction at speed :  ");
+    Serial.println(c_speed);  
+    movingBackward(c_speed);
+    }};
 
   void dec_speed(){
     c_speed = speedDecrement(c_speed);
+    if(direction_tracker == true){
+    Serial.print("Forward Direction at speed ;  ");
     Serial.println(c_speed);
     movingForward(c_speed);
+    }else if (direction_tracker == false){
+    Serial.print("Backward Direction at speed :  ");
+    Serial.println(c_speed);
+    movingBackward(c_speed);
+    }
     };
 
 void servoTest(){
@@ -149,74 +179,94 @@ void blinking(){
  for(int angle = 42; angle <=70; angle += 2) // goes from 45 degrees to 0 degrees
  { // in steps of 1 degree
  agv_servo.write(angle);
- delay(20);
+ delay(50);
  }
   };
  void turningRight(){
  for(int angle = 42; angle >=0; angle -= 2) // goes from 45 degrees to 90 degrees
  { // in steps of 1 degree
  agv_servo.write(angle); // tell servo to go to position in variable 'angle'
- delay(20);
- }}; 
+ delay(50);
+ }
+}; 
 
 
 void meanPosR(){
-  for(int angle = 0; angle <= 42; angle += 2) // goes from 90 degrees to mean postion
+  for(int angle = 0; angle <= 42; angle += 2)
  {
- agv_servo.write(angle); // move servo in opposite direction
- delay(20);
+ agv_servo.write(angle); 
+ delay(50);
  }
 };
+
 void meanPosL(){
-  for(int angle = 70; angle >= 42; angle -= 2) // goes from 0 degrees to mean position.
+  for(int angle = 70; angle >= 42; angle -= 2)
  {
- agv_servo.write(angle); // move servo in opposite direction                            
- delay(20);
+ agv_servo.write(angle);                           
+ delay(50);
  }
 };
 
   void turnOff(){
   digitalWrite(in1,LOW);
   digitalWrite(in2,LOW);
-  analogWrite(enA,0);//We shall include a for loop here. For continuous increment and decrement.
+  analogWrite(enA,0);
   };
 
  void movingForward(int c_speed){
+  direction_tracker = true; //Declares the direction as forward.
   digitalWrite(in1,LOW);
   digitalWrite(in2,HIGH);
-  analogWrite(enA,c_speed);//We shall include a for loop here. For continuous increment and decrement.
+  analogWrite(enA,c_speed);
  };
 
  void movingBackward(int b_speed){
+  direction_tracker= false; //Declares the direction as backward.
   digitalWrite(in1,HIGH);
   digitalWrite(in2,LOW);
-  analogWrite(enA,b_speed);//We shall include a for loop here. For continuous increment and decrement.
+  analogWrite(enA,b_speed);
+ };
+//
+ void direction_shifter(){
+  direction_tracker=!direction_tracker;
+  if(direction_tracker==true){
+    Serial.print("Direction toggled to Forward ");
+  }else{
+    Serial.print("Direction toggled to Backward.");
+    }
   };
-
 
 //FUNCTIONS FOR THE IR RECEIVER SERVO
 //=======================================
  void servo_left(){
- for(int angle = 90; angle <=30; angle -= 1) // goes from 45 degrees to 0 degrees
- { // in steps of 1 degree
+ for(int angle = 90; angle >=0; angle -= 1)
+ {
  IR_servo.write(angle);
- delay(20);
+ delay(10);
  }
-  obstacle_left = IR_sensor();//Servo to capture the value
-  Serial.print("A move to the left : ");
+  obstacle_left = IR_sensor();
+  Serial.print("Obstacle distance left : ");
   Serial.println(obstacle_left);
  };
 
  void servo_right(){
- for(int angle = 30; angle >=150; angle += 1) // goes from 45 degrees to 90 degrees
- { // in steps of 1 degree
- IR_servo.write(angle); // tell servo to go to position in variable 'angle'
- delay(20);
+ for(int angle = 0; angle <=180; angle += 1)
+ {
+ IR_servo.write(angle); 
+ delay(10);
  }
-   obstacle_right = IR_sensor();//Servo to capture the value
-   Serial.print("A move to the right : ");
+   obstacle_right = IR_sensor();
+   Serial.print("Obstacle distance right : ");
    Serial.println(obstacle_right);
  }; 
+
+ void servo_smooth(){
+ for(int angle = 180; angle >=90; angle -= 1)
+ { // in steps of 1 degree
+ IR_servo.write(angle);
+ delay(10);
+ }
+  };
   
  
  int IR_sensor(){
@@ -230,12 +280,12 @@ void meanPosL(){
   distance=(duration*0.034/2); 
 //  Serial.print("Distance : ");
 //  Serial.print(distance);
-//  Serial.println(" cm ");//After printing out to the screen return the distance to the obstacle variable.
+//  Serial.println(" cm ");
   return distance;
  };
 
-//BASICS FOR OBJECT DETECTION.
-//============================
+//BASICS BUZZER ACTION UPON OBJECT DETECTION.
+//===========================================
   void IR_Receiver_Module(){
   digitalWrite(trigPin,LOW);
   delayMicroseconds(2);
@@ -248,10 +298,10 @@ void meanPosL(){
   Serial.print("Distance : ");
   Serial.print(distance);
   Serial.println(" cm ");
-//  delay(500);
+  delay(500);
 
 
-  if (distance<=100){
+  if (distance<=70){
   tone(buzzerPin,2000);
   delay(50);
   turnOff();
@@ -262,8 +312,6 @@ void meanPosL(){
     meanPosL();
     turningRight();
     meanPosR();
-  
-  
     }
   else{
   movingForward(c_speed);
@@ -288,39 +336,57 @@ void buzzer_action(){//This should just be a simple buzzer test.
 //==================================================
  servo_left();
  servo_right();
+ servo_smooth();
 
 //Using the global variables to decide the fate/direction.
 //========================================================
  if(obstacle_right > obstacle_left)
     {
-movingBackward(b_speed);//Duration of time needs to be defined
-delay(500);
-Serial.print("Turning Right ");
-turningRight();
-movingForward(c_speed);
-delay(2000);
-turningLeft();
-movingForward(c_speed);
-delay(2000);
-meanPosR();
-movingForward(c_speed);
-}else if(obstacle_right < obstacle_left){
-movingBackward(b_speed);//Duration of time needs to be defined
-delay(500);
-Serial.print("Turning Left");
-turningLeft();
-movingForward(c_speed);
-delay(2000);
-turningRight();
-movingForward(c_speed);
-delay(2000);
-meanPosR();
-movingForward(c_speed);
-  }else {
-turnOff();
-for (int blk =0; blk<=5;blk++){
-    blinking();
-    delay(50);
+      if (obstacle_right<80){
+        Serial.print("Moving Backward for 200ms");
+        movingBackward(b_speed);//Duration of time needs to be defined
+        delay(200);
+        }
+        else{
+              Serial.print("Turning Right ");
+               turningRight();
+               movingForward(c_speed);
+                Serial.print("Moving Forward for 2s ");
+               delay(2000);
+               turningLeft();
+                Serial.print("Turning left ");
+               movingForward(c_speed);
+                Serial.print("Moving Forward for 2s ");
+               delay(2000);
+               Serial.print("Mean Position right.");
+               meanPosR();
+                
+                Serial.print("Moving Forward infinitely.");
+               movingForward(c_speed);
+                
+        }}
+        else if(obstacle_right < obstacle_left){
+       if (obstacle_left<=80){
+        movingBackward(b_speed);//Duration of time needs to be defined
+        delay(500);
+    }
+    else{
+
+              Serial.print("Turning Left");
+              turningLeft();
+              movingForward(c_speed);
+              delay(2000);
+              turningRight();
+              movingForward(c_speed);
+              delay(2000);
+              meanPosR();
+              movingForward(c_speed);
+  }}
+  else {
+              turnOff();
+              for (int blk =0; blk<=5;blk++){
+              blinking();
+              delay(150);
   };  
 };
 
@@ -330,58 +396,76 @@ for (int blk =0; blk<=5;blk++){
 void test (){
   
  blinking();
- delay(100);
  turningLeft();
- delay(100);
  movingForward(c_speed);
- delay(1000);
+ delay(500);
  turnOff();
- delay(100);
- meanPosL();
  delay(200);
+ meanPosL();
  movingBackward(b_speed);
- delay(1000);
+ delay(500);
  turnOff();
- delay(1000);
 
  for (int blk =0; blk<=3;blk++){
     blinking();
-    delay(100);
+    delay(50);
   };  
 
 turningRight();
-delay(100);
 movingForward(c_speed);
 delay(1000);
 turnOff();
-delay(100);
 meanPosR();
-delay(100);
 movingBackward(b_speed);
-delay(1000);
+delay(500);
 turnOff();
-delay(100);
+delay(200);
 
 };
 //SWITCH STATEMENT
 //================
 void switchCase(){
 if (irrecv.decode(&results)){
-// CODE THAT PRINTS OUT THE HEX CODE RETURNED
-//=============================================
-Serial.println(results.value,HEX);//Displays the hex results in a line.
  irrecv.resume();//THis is for the next value.
   
 if(results.value==0xFFFFFFFF){
   results.value = Previous;
 };
 switch (results.value){
-
  case Power:
  blinking();
- IR_Receiver_Module();
+ test();
  break;
-  
+
+ case Mode:
+ blinking();
+ direction_shifter();
+ break;
+ 
+ case Button_One:
+ blinking();
+ inc_speed();
+ break;
+
+ case Button_Two:
+ blinking();
+ dec_speed();
+ break;
+
+ case Button_Four:
+ blinking();
+ servo_left();
+ break;
+
+ case Button_Five:
+ blinking();
+ servo_smooth();
+ break;
+
+ case Button_Six:
+ blinking();
+ servo_right();
+ break;
 
  case Button_L:
  blinking();
@@ -412,6 +496,7 @@ switch (results.value){
  blinking();
  movingBackward(b_speed);
  break;
+ 
 };//END OF THE CASE SWITCH STATEMENT
 };
 };
@@ -446,20 +531,12 @@ pinMode(in2,OUTPUT);
 pinMode(enA,OUTPUT);//PWM
 //DEBUG SECTION
 //=============
-//servo_left();
 
-//servo_right();
-
-}
+};
 void loop(){
-//  inc_speed();
-//dec_speed();
-//Serial.print("The current speed is : ");
-//Serial.println(c_speed);
-//delay(1000);
-
-//  switchCase(); //The code is working correctly.
-// IR_Receiver_Module();
+//   buttonAssignment();
+//  switchCase(); //The code for the IR reciever
+// IR_Receiver_Module();//Code for the ultrasonic sensor.
 
 //Taking the ultrasonic sensor readings at intervals of 200ms.
 //=============================================================
@@ -484,5 +561,6 @@ if(current_time - previousTime_1>=interval){
   previousTime_1 = current_time;
   
 }
+
 
 };
